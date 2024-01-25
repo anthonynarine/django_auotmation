16d0d0339766cd76cc51e4462134020759913b3d
anthonynarine <anarine83@gmail.com>
2024-01-24 21:37:18 -0500

    Completed the greeting command to dynamically gree a user command to do this python manage.py greeting <name>

--- awd_core/dataentry/management/commands/greeting.py
+++ awd_core/dataentry/management/commands/greeting.py
@@ -1,14 +1,18 @@
+
 from django.core.management import BaseCommand
 from django.core.management.base import CommandParser
 
-# Proposed command  = python manage.py greeting
+# Proposed command  = python manage.py greeting any name
 # Proposed output  = Hi {name}, good morning
 class Command(BaseCommand):
     help = "Greet the user"
     
     def add_arguments(self, parser):
         parser.add_argument("name", type=str, help="Specifies user name")
+        # python manage.py greeting --help will display the arument and command help text
     
     def handle(self, *args, **kwargs):
         # Write your logic
-        self.stdout.write("Hi Julia, Good Morning")
+        name = kwargs["name"]
+        greeting = f"Hello {name}, good morning"
+        self.stdout.write(self.style.SUCCESS(greeting))
